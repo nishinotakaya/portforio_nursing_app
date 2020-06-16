b04c8ea4f57563f18f13bfbac81305ffc7adfcdb
EC2 Default User <ec2-user@ip-172-31-40-212.us-east-2.compute.internal>
2020-06-16 11:46:41 +0000

    指定、基本時間の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(basic_info_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
     else
-      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+      flash[:danger] = "#{@user.name}の更新は失敗しました。" + @user.errors.full_messages.join("<br>")
     end
     redirect_to users_url
   end
