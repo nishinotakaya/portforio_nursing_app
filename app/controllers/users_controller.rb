c30135b0683614e3da52a896c8af8e607ba3465b
EC2 Default User <ec2-user@ip-172-31-40-212.us-east-2.compute.internal>
2020-06-17 06:23:43 +0000

    基本情報の更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :edit_basic_info, :update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:index, :destroy, :update_basic_info, :edit_basic_info]
   before_action :set_one_month, only: :show
 
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(basic_info_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
     else
-      flash[:danger] = "#{@user.name}の更新は失敗しました。" + @user.errors.full_messages.join("<br>")
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
     end
     redirect_to users_url
   end
@@ -65,7 +65,7 @@ class UsersController < ApplicationController
     end
 
     def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
+      params.require(:user).permit(:basic_time, :work_time)
     end
     
     def set_user
