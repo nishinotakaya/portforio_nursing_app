bec5f0e0432b80d86fd7d1a442c131ecb565b2d8
EC2 Default User <ec2-user@ip-172-31-40-212.us-east-2.compute.internal>
2020-06-18 00:30:10 +0000

    編集終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:index, :destroy, :update_basic_info, :edit_basic_info]
+  before_action :admin_or_correct_user, only: :show
   before_action :set_one_month, only: :show
+  
 
   def index
     @users = User.paginate(page: params[:page]).search(params[:search])
@@ -94,6 +96,14 @@ class UsersController < ApplicationController
     # システム管理権限所有かどうか判定します。
     def admin_user
       redirect_to root_url unless current_user.admin?
+    end
+    
+    def admin_or_correct_user
+      @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end  
     end
 
 end
