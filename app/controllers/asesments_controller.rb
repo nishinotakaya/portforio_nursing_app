2e77f0590c393c23c23cedf0bb583f9b086601a5
EC2 Default User <ec2-user@ip-172-31-25-4.us-east-2.compute.internal>
2020-08-14 10:56:51 +0000

    これからアセスメント

--- app/controllers/asesments_controller.rb
+++ app/controllers/asesments_controller.rb
@@ -5,11 +5,23 @@ class AsesmentsController < ApplicationController
     @asesment = @client.asesments.new
   end
   
+  def create_asesment_basic_info
+    @asesment = @client.asesments.new(asesment_params)
+    if @asesment.save
+       flash[:success] = "アセスメントを追加しました！"
+       redirect_back_or @client
+    else
+      render asesment_new @client 
+    end  
+  end  
+  
   
   private
   
-  params.require(:asesment).permit(:asesment_create_day, :praxis_place, :filling_up, :why_assesment, :family_name, :family_joining, :family_age, :family_health, :crying_email_name, :crying_email_joining,
-  :crying_email_life, :crying_call, :first_time_reception, :reception_method, :reception_requester, :counselling_route, :life_history, :life_now, :trauma_old_people_adl, :dementia_old_people_adl, 
-  :nursing_number_day, :nursing_number_duration, :nursing_care_on_sickness_insurance, :health_insurence, :handicapped_notebook, :livelihood_protection, :nursing_care_service, :nursing_care_service_call, 
-  :nursing_care_service_content)
+  def asesment_params  
+    params.require(:asesment).permit(:asesment_create_day, :praxis_place, :filling_up, :why_assesment, :family_name, :family_joining, :family_age, :family_health, :crying_email_name, :crying_email_joining,
+    :crying_email_life, :crying_call, :first_time_reception, :reception_method, :reception_requester, :counselling_route, :life_history, :life_now, :trauma_old_people_adl, :dementia_old_people_adl, 
+    :nursing_number_day, :nursing_number_duration, :nursing_care_on_sickness_insurance, :health_insurence, :handicapped_notebook, :livelihood_protection, :nursing_care_service, :nursing_care_service_call, 
+    :nursing_care_service_content)
+  end  
 end
