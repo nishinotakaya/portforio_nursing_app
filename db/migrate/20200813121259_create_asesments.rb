d5a9c71c4a0fc0b91e069bf268d89b5d64abbc24
nishinotakaya <takaya314boxing@gmail.com>
2020-08-19 21:47:57 +0900

    アセスメント途中

--- db/migrate/20200813121259_create_asesments.rb
+++ db/migrate/20200813121259_create_asesments.rb
@@ -4,35 +4,32 @@ class CreateAsesments < ActiveRecord::Migration[5.1]
       t.datetime :asesment_create_day #作成日
       t.string :praxis_place #実行場所
       t.string :filling_up #記入者
-      t.string :why_assesment #アセスメント理由
+      t.string :new_or_continuation #新規か継続
+      t.string :insurance_number #保険番号
       t.string :client_name_japanese #利用者名ふりがな
-      t.string :family_name #家族構成名前
-      t.string :family_joining #続柄
-      t.string :family_age #年齢
-      t.string :family_health #健康状態 
-      t.string :crying_email_name #緊急連絡先名前
-      t.string :crying_email_joining #緊急続柄
-      t.string :crying_email_life #緊急住所
-      t.string :crying_call #緊急電話番号
-      t.datetime :first_time_reception #初回受付月日
-      t.string :reception_method #受付方法
-      t.string :reception_requester #依頼者
-      t.string :counselling_route #相談経路
-      t.string :life_history #生活歴
-      t.string :life_now #現在の生活状況
-      t.string :trauma_old_people_adl #障害高齢者の日常生活自立度
-      t.string :dementia_old_people_adl #認知症高齢者の日常生活自立度
-      t.datetime :nursing_number_day #認定年月日
-      t.string :nursing_number_duration #認定機関
-      t.string :nursing_care_on_sickness_insurance #介護保険
-      t.string :health_insurence #医療保険
-      t.string :handicapped_notebook #障碍者手帳
-      t.string :livelihood_protection #生活保護
-      t.string :nursing_care_service #現在利用している介護サービス
-      t.string :nursing_care_service_call #事業名、連絡先等
-      t.string :nursing_care_service_content #サービス種別
-      t.references :client, foreign_key: true
-
+      t.string :crying_email #世帯携帯
+      t.string :relation_name #上記以外の連絡先の方の名前
+      t.string :relation_email #上記以外の住所
+      t.string :relation_joining #本人との続柄続柄
+      t.string :relation_cellphone #上記以外の電話番号
+      t.string :family_health_disease #健康状態現在の疾患
+      t.string :family_health_disease_past#健康状態過去の疾患
+      t.string :eyesight #視力
+      t.string :audition #聴力
+      t.string :prosthesis #義歯
+      t.string :nomal_vital_body_temperature #平常時バイタル 体温
+      t.string :nomal_vital_blood_pressure #平常時血圧
+      t.string :nomal_vital_pulse #脈
+      t.string :medicina #配薬名
+      t.string :information_medicina #配薬情報源
+      t.string :hospital_name #かかりつけ医
+      t.string :hospital_doctor #医者
+      t.string :hospital_doctor_nymber #病院電話番号
+      t.string :doctor_bath #医者の指示（入浴)
+      t.string :doctor_skin #医者の指示(皮膚疾患)
+      t.string :doctor_transfer #移送時留意点
+      t.string :doctor_etcetera #その他制限
+      t.string :specialized_equipment #特機事項
       t.timestamps
     end
   end
