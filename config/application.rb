ae0cf0bc21a9d8dfef4fd55be2444d37e2c274ca
takaya <takaya314boxing@gmail.com>
2020-09-19 19:31:19 +0900

    heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module AttendanceApp
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.web_console.development_only = false
     #config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
