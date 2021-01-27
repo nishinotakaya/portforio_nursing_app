0c424f7d9b23436dac40115287b08d068dbfaa53
nishino <takaya314boxing@gmail.com>
2021-01-28 03:05:24 +0900

    fix:シフト表

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -3,20 +3,22 @@ class ShiftsController < ApplicationController
 
 	def staff_sfift
 		@staffs = Staff.all
-		@first_day = Date.current.beginning_of_month
+		@first_day =  params[:date].nil? ? 
+		Date.current.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
 		@shifts = Shift.where(shift_day: @first_day..@last_day).order(:staff_id, :shift_day)
 	end	
 
 
 	def shifts_change
-		@first_day = Date.current.beginning_of_month
+		@first_day =  params[:date].nil? ? 
+		Date.current.beginning_of_month : params[:date].to_date
 		@last_day = @first_day.end_of_month
 		@staffs = Staff.where(check_shift: true)
 	end
 	
 	def shifts_update
-		
+		@staffs = Staff.where(check_shift: true)
 	end	
 end
 
